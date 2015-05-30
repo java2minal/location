e1ac2173c42970eb1f9848c86afd9f4022b12382
java2minal <minal_saeed@hotmail.com>
2015-05-30 13:31:58 +0800

    fadheelah

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.fadheelahdaham.location;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    Button btnShowLocation;
+
+    GPSTracker gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowLocation = (Button) findViewById(R.id.show_location);
+
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gps = new GPSTracker(MainActivity.this);
+
+                if (gps.canGetLocation){
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    Toast.makeText(getApplicationContext(),"Your Location is -\nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }
+                else {
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+    }
+
+
+}
